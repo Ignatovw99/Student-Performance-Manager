696cad7012c0f8d2c6e17a258cbd277a85da2005
Ignatovw99 <lyuboslavw@gmail.com>
2022-08-13 16:33:11 +0300

    base entity class with id property created

---
+++ src/main/java/bg/nbu/sms/common/IdEntity.java
@@ -0,0 +1,17 @@
+package bg.nbu.sms.common;
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Data
+public class IdEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
