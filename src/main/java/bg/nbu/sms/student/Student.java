99b3a2b39c204c8a61021303fa39807a58862cae
Ignatovw99 <lyuboslavw@gmail.com>
2022-08-14 12:48:50 +0300

    school class removed from student entity

--- src/main/java/bg/nbu/sms/student/Student.java
+++ src/main/java/bg/nbu/sms/student/Student.java
@@ -1,14 +1,15 @@
 package bg.nbu.sms.student;
 
 import bg.nbu.sms.auth.entity.User;
-import bg.nbu.sms.common.IdEntity;
 import bg.nbu.sms.parent.Parent;
 import bg.nbu.sms.school.School;
-import bg.nbu.sms.school.SchoolClass;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
@@ -19,9 +20,6 @@ public class Student extends User {
 
     @ManyToOne
     private School school;
-
-    @Enumerated(EnumType.STRING)
-    private SchoolClass schoolClass;
 
     @ManyToMany(mappedBy = "children")
     private Set<Parent> parents;
